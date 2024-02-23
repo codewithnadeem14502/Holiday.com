dce35d2758ac2c7508aed3172e53d3cc34c15c94
codewithnadeem14502 <codewithnadeem@14502>
2024-02-23 16:31:48 +0530

    Filter is done

--- client/src/api-clients.ts
+++ client/src/api-clients.ts
@@ -116,24 +116,39 @@ export const updateMyHotelById = async (hotelFromData: FormData) => {
 };
 
 export type SearchParams = {
-  destination: string;
-  checkIn: string;
-  checkOut: string;
-  adultCount: string;
-  childCount: string;
-  page: string;
+  destination?: string;
+  checkIn?: string;
+  checkOut?: string;
+  adultCount?: string;
+  childCount?: string;
+  page?: string;
+  facilities?: string[];
+  types?: string[];
+  stars?: string[];
+  maxPrice?: string;
+  sortOption?: string;
 };
 
-export const SearchHotels = async (
-  SearchParams: SearchParams
+export const searchHotels = async (
+  searchParams: SearchParams
 ): Promise<HotelSearchResponse> => {
   const queryParams = new URLSearchParams();
-  queryParams.append("destination", SearchParams.destination || " ");
-  queryParams.append("checkIn", SearchParams.checkIn || " ");
-  queryParams.append("checkOut", SearchParams.checkOut || " ");
-  queryParams.append("adultCount", SearchParams.adultCount || " ");
-  queryParams.append("childCount", SearchParams.childCount || " ");
-  queryParams.append("page", SearchParams.page || " ");
+  queryParams.append("destination", searchParams.destination || "");
+  queryParams.append("checkIn", searchParams.checkIn || "");
+  queryParams.append("checkOut", searchParams.checkOut || "");
+  queryParams.append("adultCount", searchParams.adultCount || "");
+  queryParams.append("childCount", searchParams.childCount || "");
+  queryParams.append("page", searchParams.page || "");
+
+  queryParams.append("maxPrice", searchParams.maxPrice || "");
+  queryParams.append("sortOption", searchParams.sortOption || "");
+
+  searchParams.facilities?.forEach((facility) =>
+    queryParams.append("facilities", facility)
+  );
+
+  searchParams.types?.forEach((type) => queryParams.append("types", type));
+  searchParams.stars?.forEach((star) => queryParams.append("stars", star));
 
   const response = await fetch(
     `${API_BASE_URL}/api/hotels/search?${queryParams}`
@@ -142,5 +157,6 @@ export const SearchHotels = async (
   if (!response.ok) {
     throw new Error("Error fetching hotels");
   }
+
   return response.json();
 };
