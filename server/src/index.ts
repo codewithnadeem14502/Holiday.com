1ad1349249e7f205a9b1bcbb717e9b42bb994552
Mohd Nadeem <105824474+codewithnadeem14502@users.noreply.github.com>
2024-03-22 07:01:49 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,69 @@
+// import express, { Request, Response } from "express";
+// import cors from "cors";
+// import mongoose from "mongoose";
+// import "dotenv/config";
+// import userRoutes from "./routes/users";
+// import authRoutes from "./routes/auth";
+// import cookieparser from "cookie-parser";
+// import path from "path";
+// import { v2 as cloudinary } from "cloudinary";
+// import myHotelsRoutes from "./routes/my-hotel";
+// import hotelRoutes from "./routes/hotels";
+// const PORT = 3000;
+// import bookingRoutes from "./routes/my-bookings";
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_SECRET,
+// });
+
+// mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+//   console.log("Database is connected");
+// });
+
+// const app = express();
+// app.use(cookieparser());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEN_URL,
+//     credentials: true,
+//   })
+// );
+// // static point
+// // app.use(express.static(path.join(__dirname, "../../client/dist")));
+// // routes end points
+// app.use("/api/users", userRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/my-hotels", myHotelsRoutes);
+// app.use("/api/hotels", hotelRoutes);
+// app.use("/api/my-bookings", bookingRoutes);
+
+// // app.get("*", (req: Request, res: Response) => {
+// //   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+// // });
+// app.get("*", (req: Request, res: Response) => {
+//   res.send("Holiday backend server is working ");
+// });
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+//   console.log(process.env.FRONTEN_URL);
+// });
 import express, { Request, Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
-import cookieparser from "cookie-parser";
-import path from "path";
+import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 import myHotelsRoutes from "./routes/my-hotel";
 import hotelRoutes from "./routes/hotels";
-const PORT = 3000;
 import bookingRoutes from "./routes/my-bookings";
+
+const PORT = 3000;
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -22,30 +75,44 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
 });
 
 const app = express();
-app.use(cookieparser());
+
+// Body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// CORS middleware
 app.use(
   cors({
     origin: process.env.FRONTEN_URL,
     credentials: true,
   })
 );
-// static point
-// app.use(express.static(path.join(__dirname, "../../client/dist")));
-// routes end points
+
+// Cookie parsing middleware
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// Set sameSite attribute for cookies
+app.use((req, res, next) => {
+  res.cookie("sameSite", "none", {
+    sameSite: "none", // Set sameSite attribute to none for cross-domain cookies
+    secure: true, // Ensure cookies are only sent over HTTPS
+  });
+  next();
+});
+
+// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/my-hotels", myHotelsRoutes);
 app.use("/api/hotels", hotelRoutes);
 app.use("/api/my-bookings", bookingRoutes);
 
-// app.get("*", (req: Request, res: Response) => {
-//   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
-// });
+// Default route
 app.get("*", (req: Request, res: Response) => {
-  res.send("Holiday backend server is working ");
+  res.send("Holiday backend server is working");
 });
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(process.env.FRONTEN_URL);
