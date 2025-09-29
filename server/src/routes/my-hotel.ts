aef33b90b9a1d34b92ad72085faa1fbb1a9b2c20
codewithnadeem14502 <codewithnadeem@gmail.com>
2025-09-30 00:11:49 +0530

    fix

--- server/src/routes/my-hotel.ts
+++ server/src/routes/my-hotel.ts
@@ -134,7 +134,7 @@ router.delete("/:hotelId", verifyToken, async (req: Request, res: Response) => {
 
 async function uploadImages(imageFiles: Express.Multer.File[]) {
   const uploadPromises = imageFiles.map(async (image) => {
-    const b64 = Buffer.from(image.buffer).toString("base64");
+    const b64 = image.buffer.toString("base64");
     let dataURI = "data:" + image.mimetype + ";base64," + b64;
     const res = await cloudinary.v2.uploader.upload(dataURI);
     return res.url;
